32957ad8c893d2d065fe93e3a35ec42ae4f1119c
david <devdavid1307@gmail.com>
2020-12-31 11:22:05 +0800

    create add function

---
+++ go.mod
@@ -0,0 +1,5 @@
+module david.tips/actions
+
+go 1.15
+
+require github.com/stretchr/testify v1.6.1
---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+func TestAdd(t *testing.T) {
+    a := 1
+    b := 1
+
+    expected := a+b
+
+    result := Add(a, b)
+
+    assert.Equal(t, expected, result)
+}
