e362df666197c1cf19c660d6ae119819302d02c3
david <devdavid1307@gmail.com>
2021-01-13 15:38:16 +0800

    update action

--- .github/actions/setup-env/src/setup.ts
+++ .github/actions/setup-env/src/setup.ts
@@ -5,6 +5,7 @@ export async function setup(): Promise<void> {
         const version = core.getInput('php-version')
 
         console.log(version)
+        console.log(process.platform)
     } catch(error) {
         core.setFailed(error.message)
     }
