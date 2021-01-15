41853fe351f5f45065a28dc97ee18f2f4b3276d5
david <devdavid1307@gmail.com>
2021-01-15 14:57:51 +0800

    php env

---
+++ .github/actions/setup-unit-test/src/scripts/common.sh
@@ -0,0 +1,21 @@
+export tick="✓"
+export cross="✗"
+
+# Function to log start of a operation.
+step_log() {
+  message=$1
+
+  printf "\n\033[90;1m==> \033[0m\033[37;1m%s\033[0m\n" "$message"
+}
+
+# Function to log result of a operation.
+add_log() {
+  mark=$1
+  subject=$2
+  message=$3
+  if [ "$mark" = "$tick" ]; then
+    printf "\033[32;1m%s \033[0m\033[34;1m%s \033[0m\033[90;1m%s\033[0m\n" "$mark" "$subject" "$message"
+  else
+    printf "\033[31;1m%s \033[0m\033[34;1m%s \033[0m\033[90;1m%s\033[0m\n" "$mark" "$subject" "$message" && exit 1
+  fi
+}
--- .github/actions/setup-unit-test/src/scripts/install.sh
+++ .github/actions/setup-unit-test/src/scripts/install.sh
@@ -1,7 +1,13 @@
 setup() {
-   echo "php版本: ${version}"
+    step_log "Setup PHP"
+
+    add_log "${tick:?}" "$version"
 }
 
+# 获取参数
 version=$1
+
+# 加载common.sh
+. ./common.sh
 
 setup
