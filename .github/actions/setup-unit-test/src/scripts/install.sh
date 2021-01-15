6739c8e5abfd15de5168e63518d76ff9527f4fd2
david <devdavid1307@gmail.com>
2021-01-15 17:49:44 +0800

    php env

--- .github/actions/setup-unit-test/src/scripts/install.sh
+++ .github/actions/setup-unit-test/src/scripts/install.sh
@@ -40,9 +40,10 @@ add_pecl_extension() {
 
     # 添加到php.ini
     ini_dir=$(php --ini | grep "(php.ini)" | sed -e "s|.*: s*||")
-    echo "$ini_dir"
 
-    echo "extension=$ext.so" | sudo tee -a "${ini_dir}" >/dev/null
+    sudo ls "$ini_dir"
+
+    echo "extension=$ext.so" | sudo tee -a "${ini_dir}/php.ini" >/dev/null
 
     php -m
 }
