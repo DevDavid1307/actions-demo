7727179bbd3a30b821a890cfe7e6527caa3056ec
david <devdavid1307@gmail.com>
2021-01-15 14:28:54 +0800

    php env

--- .github/actions/setup-unit-test/src/install.ts
+++ .github/actions/setup-unit-test/src/install.ts
@@ -1,7 +1,7 @@
 import * as exec from '@actions/exec'
 
 export async function run(): Promise<void> {
-    await exec.exec('bssh', ['../scripts/install.sh'])
+    await exec.exec('bash', ['../scripts/install.sh'])
 }
 
 run()
