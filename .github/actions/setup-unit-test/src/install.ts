19fe15f238a9c9283fc96ce7309798c60a9fcc51
david <devdavid1307@gmail.com>
2021-01-15 14:49:04 +0800

    php env

--- .github/actions/setup-unit-test/src/install.ts
+++ .github/actions/setup-unit-test/src/install.ts
@@ -1,10 +1,11 @@
 import * as exec from '@actions/exec'
 import * as utils from './utils'
+import path from "path";
 
 export async function run(): Promise<void> {
-    const script = await utils.readScript("install.sh")
+    // const script = await utils.readScript("install.sh")
 
-    await exec.exec('bash', [script])
+    await exec.exec('bash', [path.join(__dirname, "../src/scripts/install.sh")])
 }
 
 run()
